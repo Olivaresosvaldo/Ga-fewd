f20e5d681f26e9286fc4b42b59d5af670430b073
Olivaresosvaldo <olivares@Osvaldos-MacBook-Pro.local>
2017-04-06 01:18:33 -0400

    D13

---
+++ d14/Ideal-Image-Slider/js/my.js
@@ -0,0 +1,14 @@
+// var myslider = new IdealImageSlider.Slider({
+//   selector: '#slider',
+//   onInit: function(){
+//     console.log('onInit');
+//   }
+// });
+// myslider.start();
+
+var slider = new IdealImageSlider.Slider({
+  selector: '#slider',
+  height: 400,
+  interval: 4000
+});
+slider.start();
